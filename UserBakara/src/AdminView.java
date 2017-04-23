19f99062a8396e1e7810462a449ae936dbace9a1
Max <Max@Max-PC>
2017-04-24 00:54:34 +0300

    seccond commit

--- UserBakara/src/AdminView.java
+++ UserBakara/src/AdminView.java
@@ -61,6 +61,7 @@ public class AdminView {
 	
 	public static void updateTable() throws SQLException
 	{
+		//Second commit!
 		String query = "select * from EmployeeInfo";
 		stmt = conn.createStatement();
 		resSet = stmt.executeQuery(query);
